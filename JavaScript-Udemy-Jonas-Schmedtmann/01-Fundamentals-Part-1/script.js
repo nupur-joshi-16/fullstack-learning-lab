1179f9e69d6d732eb08410c333d43077e1ce2a06
nupur-joshi-16 <nupur.p.joshi@outlook.com>
2026-05-18 08:27:07 +0530

    Day 1
    Setting up code editor, hello world, JavaScript file linking, variables, data types, let/const/var, basic operators and operator precedence.

---
+++ JavaScript-Udemy-Jonas-Schmedtmann/01-Fundamentals-Part-1/script.js
@@ -0,0 +1,57 @@
+// let js = "amazing";
+// if (js === "amazing") alert("JavaScript is fun.");
+
+// console.log(10 + 20 + 30);
+
+// console.log("Nupur");
+// console.log(42);
+
+// Variables
+
+// let firstName = "Nupur";
+// let userAge = 42;
+// console.log(firstName, userAge);
+// console.log(firstName);
+// console.log(userAge);
+// console.log(userAge, firstName);
+
+// let myFisrtJob = "UI/UX Designer";
+// let myCurrentJob = "AEM UI Developer";
+// console.log(myFisrtJob, myCurrentJob);
+
+// Data Types
+
+// console.log(typeof 'Nupur');
+// console.log(typeof 42);
+// console.log(typeof true);
+// let future;
+// console.log(typeof future);
+// console.log(typeof null);
+
+// var, let, const
+
+// Basic operators
+
+// Assignment operators
+const userBirthYear = 2001;
+const currentYear = 2026;
+const userAge = currentYear - userBirthYear;
+console.log(userAge);
+
+// +, - , *, /, **, %, +=, *=, /=, ++, --
+
+console.log(2 ** 10);
+console.log(20 % 3);
+
+// Comparison operators
+
+// >, <, >=, <=, 
+
+console.log(10 > 2);
+
+
+
+
+
+
+
