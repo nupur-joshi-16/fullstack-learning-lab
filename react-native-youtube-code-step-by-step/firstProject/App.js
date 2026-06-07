2951220d3218070e9db425ed2ce0fbcd2a3cac24
nupur-joshi-16 <nupur.p.joshi@outlook.com>
2026-06-07 09:05:46 +0530

    Day 1
    Set up React Native with Expo on Linux.
    
    Created the first JavaScript-based Expo project.
    Configured web support.
    Ran the app successfully on Expo Go.
    Built the first Hello World app in App.js.

---
+++ react-native-youtube-code-step-by-step/firstProject/App.js
@@ -0,0 +1,19 @@
+// import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>Hello world!</Text>
+    </View>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
