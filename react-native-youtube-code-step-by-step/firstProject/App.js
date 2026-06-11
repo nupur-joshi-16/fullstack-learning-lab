2cfec910117cbf0580ffe426e43028097b0eab1f
nupur-joshi-16 <nupur.p.joshi@outlook.com>
2026-06-11 07:34:19 +0530

    State in react-native

--- react-native-youtube-code-step-by-step/firstProject/App.js
+++ react-native-youtube-code-step-by-step/firstProject/App.js
@@ -1,19 +1,66 @@
-// import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { Text, View, Button } from "react-native";
+import CompanyData from "./components/CompanyData";
 
 export default function App() {
+  let name = "Nupur";
+  let age = 42;
+  let email = "demo@mail.com";
+
+  const [firstName, setFirstName] = useState("Ram");
+
+  function testName() {
+    setFirstName("Shyam");
+  }
+
+  let message;
+
+  function fruit() {
+    console.warn("Function called!");
+  }
+
+  if (age === 42) {
+    message = "You should learn AI tools";
+  }
+
   return (
     <View>
-      <Text style={{ fontSize: 30 }}>Hello world!</Text>
+      <Text style={{ fontSize: 20 }}>Good morning,</Text>
+      <Text style={{ fontSize: 32 }}>Nupur</Text>
+      <Text style={{ fontSize: 14, fontWeight: "700" }}>
+        What are you doing today?
+      </Text>
+
+      <Button title="Button" />
+
+      <Text>
+        {name}, {age}, {email}
+      </Text>
+
+      <Text>{message}</Text>
+
+      <UserData />
+      <UserData />
+      <UserData />
+      <UserData />
+
+      <CompanyData />
+
+      <Button title="Button" onPress={fruit} color="#000" />
+
+      <Text>{firstName}</Text>
+      <Button title="Update Name" onPress={testName}></Button>
     </View>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+const UserData = () => {
+  let country = "India";
+
+  return (
+    <View>
+      <Text>Name : Nupur</Text>
+      <Text>{country}</Text>
+    </View>
+  );
+};
---
+++ react-native-youtube-code-step-by-step/firstProject/components/CompanyData.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const CompanyData = () => {
+  return (
+    <View>
+      <Text>Company Name : Zoho</Text>
+    </View>
+  );
+};
+
+export default CompanyData;
